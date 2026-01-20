d3a793755e54feca97dde4aed366bc250ce0addd
Stone <comstockstone@yahoo.com>
2026-01-20 00:25:35 -0800

    posts schema for db

---
+++ backend/models/Posts.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 1000,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+});
+
+module.exports = mongoose.model("Post", postSchema);
