c9dcb964303344691fa4f0bb0045e7c6425e5b5d
Stone <comstockstone@yahoo.com>
2026-01-20 00:35:54 -0800

    added auth routes to server file, forgot earlier

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const authRoutes = require('./routes/auth');
 
 const app = express();
+app.use('/api/auth', authRoutes);
 
 // Middleware
 app.use(cors());
